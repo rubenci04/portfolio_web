d8a07f9e0bf37a036749391ace405f97f08f3059
rubenci04 <rea_04@hotmail.com.ar>
2025-09-28 16:26:49 -0300

    mensaje

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Background.css';
+
+const Background = () => {
+  return (
+    <div className="background">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </div>
+  );
+};
+
+export default Background;
